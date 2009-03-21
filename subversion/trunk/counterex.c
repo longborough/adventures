4e377ab6b64fa375362f64e68f3e36b92c30aa17
brent <brent.github@quadrangle.dyndns.org>
2009-03-21 19:37:25 +0000

    svn: Update (b1) to (b2) and commit

--- subversion/trunk/counterex.c
+++ subversion/trunk/counterex.c
@@ -1,3 +1,10 @@
+int square(int x) {int y = x;
+	/* This does some squaring */
+	/* Update y to equal the result. */
+	/* Question: what is the order of magnitude of this algorithm with respect to x? */
+	for (int i = 0; i < x; i++) y += x; return y;
+}
+
 int very_slow_square(int x) {int y;
 	y = 0;
 	/* Update y to equal the result. */
@@ -8,7 +15,7 @@ int very_slow_square(int x) {int y;
 	return y;
 }
 
-int square(int x) {int y = x;
+int slow_square(int x) {int y = -3*x*x; x *= 2;
 	/* This does some squaring */
 	/* Update y to equal the result. */
 	/* Question: what is the order of magnitude of this algorithm with respect to x? */
