c679cbd5392b6ac92f84a1099bd3d17572385218
brent <brent.github@quadrangle.dyndns.org>
2009-03-21 19:44:23 +0000

    svn: Update cbranch from (a) to (c1) and commit

--- subversion/branches/cbranch/counterex.c
+++ subversion/branches/cbranch/counterex.c
@@ -1,5 +1,5 @@
 int square(int x) {int y = x;
-	/* This does some squaring */
+	y = 0;
 	/* Update y to equal the result. */
 	/* Question: what is the order of magnitude of this algorithm with respect to x? */
 	for (int i = 0; i < x; i++) y += x;
