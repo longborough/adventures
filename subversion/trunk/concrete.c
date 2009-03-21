7ee5b16a25d253a593ce0c0652f54b489825961e
brent <brent.github@quadrangle.dyndns.org>
2009-03-21 19:28:56 +0000

    svn: Update trunk from (a) to (b1) and committed

--- subversion/trunk/concrete.c
+++ subversion/trunk/concrete.c
@@ -1,3 +1,13 @@
+int very_slow_square(int x) {
+	int y = 0;
+	/* Update y to equal the result. */
+	/* Question: what is the order of magnitude of this algorithm with respect to x? */
+	for (int i = 0; i < x; i++)
+		for (int j = 0; j < x; j++)
+			y += 1;
+	return y;
+}
+
 int square(int x) {
 	int y = x;
 	/* Update y to equal the result. */
